2677f8c2c43db5e53883aa5a1dd543faa2c3007f
Chakhguereev <lioma.chakhguereev.sio@gmail.com>
2023-06-07 16:51:51 +0200

    effet selont pc ou smartphone

--- public/js/cv_responsive_animations.js
+++ public/js/cv_responsive_animations.js
@@ -17,19 +17,29 @@ $(function () {
 });
 
 function initial_view(){
-      $("#about").hide();    
+      $("#about").hide(); 
       $("#skills").hide();
       $("#experience").hide();
       $("#education").hide();
       $("#portfolio").hide();
       $("#about").fadeIn();
+      $("#about_butt").addClass('active');
 };
 
 
 $(document).ready(function(){
+    console.log($( window ).width());
+    if (window.matchMedia("(min-width: 1281px)").matches) {
+        
     initial_view();
     $("#about_butt").click(function(){
+        $("#about_butt").addClass('active');
+        $("#skills_butt").removeClass('active');
+        $("#experience_butt").removeClass('active');
+        $("#education_butt").removeClass('active');
+        $("#portfolio_butt").removeClass('active');
         $("#about").fadeIn();
+        
       $("#skills").hide();
       $("#experience").hide();
       $("#education").hide();
@@ -37,6 +47,11 @@ $(document).ready(function(){
     });
 
     $("#skills_butt").click(function(){
+        $("#skills_butt").addClass('active');
+        $("#about_butt").removeClass('active');
+        $("#experience_butt").removeClass('active');
+        $("#education_butt").removeClass('active');
+        $("#portfolio_butt").removeClass('active');
         $("#skills").fadeIn();
         $("#about").hide();
         $("#experience").hide();
@@ -45,6 +60,11 @@ $(document).ready(function(){
     });
 
     $("#experience_butt").click(function(){
+        $("#experience_butt").addClass('active');
+        $("#about_butt").removeClass('active');
+        $("#skills_butt").removeClass('active');
+        $("#education_butt").removeClass('active');
+        $("#portfolio_butt").removeClass('active');
         $("#experience").fadeIn();
         $("#about").hide();
         $("#skills").hide();
@@ -53,6 +73,11 @@ $(document).ready(function(){
     });
 
     $("#education_butt").click(function(){
+        $("#education_butt").addClass('active');
+        $("#about_butt").removeClass('active');
+        $("#skills_butt").removeClass('active');
+        $("#experience_butt").removeClass('active');
+        $("#portfolio_butt").removeClass('active');
         $("#education").fadeIn();
         $("#about").hide();
         $("#skills").hide();
@@ -61,10 +86,21 @@ $(document).ready(function(){
     });
 
     $("#portfolio_butt").click(function(){
+        $("#portfolio_butt").addClass('active');
+        $("#about_butt").removeClass('active');
+        $("#skills_butt").removeClass('active');
+        $("#experience_butt").removeClass('active');
+        $("#education_butt").removeClass('active');
         $("#portfolio").fadeIn();
         $("#about").hide();
         $("#skills").hide();
         $("#experience").hide();
         $("#education").hide();
     });
+}
+
   });
+
+
+  
+  
