153562325239f83dc3c179ef2206f71a95d2cce9
Chakhguereev <lioma.chakhguereev.sio@gmail.com>
2023-07-17 12:36:19 +0200

    correction programmes view divice

--- public/js/programmes.js
+++ public/js/programmes.js
@@ -3,10 +3,6 @@ jQuery(document).ready(function ($) {
         window.location = $(this).data("href");
     });
 
-    $(function () {
-        $('#table').bootstrapTable()
-    })
-
     $("#rest").on('click', function () {
         // alert("my message to you");
         $("#rest").addClass("d-none");
