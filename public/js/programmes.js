da30e05206211aee85b2babf286c84afdd7d5de1
Chakhguereev <lioma.chakhguereev.sio@gmail.com>
2023-12-06 10:04:09 +0100

    Maj chrono

--- public/js/programmes.js
+++ public/js/programmes.js
@@ -3,55 +3,56 @@ jQuery(document).ready(function ($) {
         window.location = $(this).data("href");
     });
 
-    $("#rest").on('click', function () {
-        $("#rest").addClass("d-none");
-
-
-        $timer = $('.timer').startTimer({
-            onComplete: function (element) {
-                $('html, body').addClass('bodyTimeoutBackground');
-            },
-
-        }).on('click', function () {
-
-            $(".jst-hours").remove();
-            $(".jst-minutes").remove();
-            $(".jst-seconds").remove();
-            $("#next").addClass("d-none");
-            $("#rest").removeClass("d-none");
-
-        });
+    //chronomètre de récupération
+    let chrono = false;
+    const rest = document.querySelector('#rest');
+    rest.addEventListener("click", (e) => {
+        if (!chrono) {
+            let reinit = rest.textContent;
+            let time = parseInt(rest.innerText.split(' ', 2)[1]);
+            chrono = true;
+            const timerId = setInterval(() => {
+                rest.innerHTML = time--;
+                    if (time <= -1) {
+                        chrono = false;
+                        rest.innerHTML = reinit;
+                        clearInterval(timerId);
+                    }
+                },
+                1000);
+        }
     });
 
 
-});
-
 
 //vibration
-$(function() {
-    var interval = 30;
-    var duration= 1000;
-    var shake= 3;
-    var vibrateIndex = 0;
-    var selector = $('#frame');
-    $('#activate').click(
-        function(){
-            // navigator.vibrate(200);
-            vibrateIndex = setInterval(vibrate, interval);
-            setTimeout(stopVibration, duration);
-        });
+    $(function () {
+        var interval = 30;
+        var duration = 1000;
+        var shake = 3;
+        var vibrateIndex = 0;
+        var selector = $('#frame');
+        $('#activate').click(
+            function () {
+                // navigator.vibrate(200);
+                vibrateIndex = setInterval(vibrate, interval);
+                setTimeout(stopVibration, duration);
+            });
 
-    var vibrate = function(){
-        $(selector).stop(true,false)
-            .css({position: 'relative',
-                left: Math.round(Math.random() * shake) - ((shake + 1) / 2) +'px',
-                top: Math.round(Math.random() * shake) - ((shake + 1) / 2) +'px'});
-    }
+        var vibrate = function () {
+            $(selector).stop(true, false)
+                .css({
+                    position: 'relative',
+                    left: Math.round(Math.random() * shake) - ((shake + 1) / 2) + 'px',
+                    top: Math.round(Math.random() * shake) - ((shake + 1) / 2) + 'px'
+                });
+        }
 
-    var stopVibration = function() {
-        clearInterval(vibrateIndex);
-        $(selector).stop(true,false)
-            .css({position: 'static', left: '0px', top: '0px'});
-    };
+        var stopVibration = function () {
+            clearInterval(vibrateIndex);
+            $(selector).stop(true, false)
+                .css({position: 'static', left: '0px', top: '0px'});
+        };
 
+    });
 });
