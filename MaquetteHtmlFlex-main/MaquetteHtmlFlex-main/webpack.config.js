d63d58ccc8fc4e635923a8b17ef78340b65189a7
Chakhguereev <lioma.chakhguereev.sio@gmail.com>
2023-12-08 15:17:51 +0100

    MaJ du design + mediaQueries

---
+++ MaquetteHtmlFlex-main/MaquetteHtmlFlex-main/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname, "./src/index.js")
+  },
+  output: {
+    path: path.join(__dirname, "./dist"),
+    filename: "[name].bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /(node_modules)/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "./src/index.html")
+    })
+  ],
+  stats: "minimal",
+  devtool: "source-map",
+  mode: "development",
+  devServer: {
+    open: false,
+    static: path.resolve(__dirname, './dist'),
+    watchFiles: ['./src/**'],
+    port: 4000,
+    hot: true,
+  }
+};
